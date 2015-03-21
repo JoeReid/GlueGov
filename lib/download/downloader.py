bf289b72c51a550b0a2d776d66fdd370603894ba
JoeReid <jcreid.computing@gmail.com>
2015-03-21 16:16:36 +0000

    Now using requests rather than urllib

--- lib/download/downloader.py
+++ lib/download/downloader.py
@@ -1,6 +1,10 @@
-import urllib.request
+import requests
+# import concurrent.futures
 
 
 class Downloader(object):
+    # downloadExecutor = concurrent.futures.ThreadPoolExecutor(max_workers=5)
+
     def __init__(self, url, dest):
-        urllib.request.urlretrieve(url, dest)
+        response = requests.get(url)
+        open(dest, 'wb').write(response.content)
