51189d115b5d6677b816418a3ba496a5c0efd8ef
JoeReid <jcreid.computing@gmail.com>
2015-03-22 00:11:45 +0000

    All the datagit statusgit status

--- server/gluegov/data/dclg.py
+++ server/gluegov/data/dclg.py
@@ -24,3 +24,27 @@ XLSTable(
     "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/6875/1871537.xls",
     "healthdomain.xls"
 ).parse(sheet=1, lastRow=32483)
+XLSTable(
+    "dclg",
+    "skillstrainingdomain",
+    "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/6876/1871555.xls",
+    "skillstrainingdomain.xls"
+).parse(sheet=1, lastRow=32483)
+XLSTable(
+    "dclg",
+    "housingandservicebarriers",
+    "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/6877/1871559.xls",
+    "housingandservicebarriers.xls"
+).parse(sheet=1, lastRow=32483)
+XLSTable(
+    "dclg",
+    "livingenvironmentdomain",
+    "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/6879/1871567.xls",
+    "livingenvironmentdomain.xls"
+).parse(sheet=1, lastRow=32483)
+XLSTable(
+    "dclg",
+    "criminaldomain",
+    "https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/6878/1871562.xls",
+    "criminaldomain.xls"
+).parse(sheet=1, lastRow=32483)
