9de19d120345b8517337af235b770bd7595a3eb4
Richard Lancaster <richard@rl0.co.uk>
2015-03-22 10:45:38 +0000

    Support spaces in queries

--- server/gluegov/lib/tables.py
+++ server/gluegov/lib/tables.py
@@ -3,8 +3,10 @@
 Using the word table, due to lack of a better idea!
 """
 import os
+import urllib.parse
 import configparser
 
+from functools import partial
 from collections import defaultdict
 from gluegov.lib.parsers import CSVParser
 from gluegov.lib.parsers import XLSParser
@@ -17,12 +19,15 @@ config.read("development.ini")
 class Table(object):
     SUPPORTED_FUNCS = ["eq", "gt", "lt", "gte", "lte", "neq", "con"]
 
-    def __init__(self, records):
+    def __init__(self, fields, records):
         self.records = records
-        self.fields = []
+        self.fields = fields
+
+        self.tablePartial = None
+
 
     def _filter(self, func, value):
-        types = [str, int, float]
+        types = [str, urllib.parse.unquote, int, float]
         for t in types:
             try:
                 t(value)
@@ -35,29 +40,30 @@ class Table(object):
         return [record for record in self.records if func(record, value, type)]
 
     def eq(self, field, value):
-        return Table(self._filter(lambda d, v, t: t(d[field]) == v, value))
+        return self.tablePartial(self._filter(lambda d, v, t: t(d[field]) == v, value))
 
     def gt(self, field, value):
-        return Table(self._filter(lambda d, v, t: t(d[field]) > v, value))
+        return self.tablePartial(self._filter(lambda d, v, t: t(d[field]) > v, value))
 
     def lt(self, field, value):
-        return Table(self._filter(lambda d, v, t: t(d[field]) < v, value))
+        return self.tablePartial(self._filter(lambda d, v, t: t(d[field]) < v, value))
 
     def gte(self, field, value):
-        return Table(self._filter(lambda d, v, t: t(d[field]) >= v, value))
+        return self.tablePartial(self._filter(lambda d, v, t: t(d[field]) >= v, value))
 
     def lte(self, field, value):
-        return Table(self._filter(lambda d, v, t: t(d[field]) <= v, value))
+        return self.tablePartial(self._filter(lambda d, v, t: t(d[field]) <= v, value))
 
     def neq(self, field, value):
-        return Table(self._filter(lambda d, v, t: t(d[field]) != v, value))
+        return self.tablePartial(self._filter(lambda d, v, t: t(d[field]) != v, value))
 
     def con(self, field, value):
         def contains(d, v, t):
             return v in t(d[field]).lower()
-        return Table(self._filter(contains, value))
+        return self.tablePartial(self._filter(contains, value))
 
     def proccesQuery(self, query):
+        self.tablePartial = partial(Table, self.fields)
         table = self
 
         if query:
@@ -86,7 +92,7 @@ class TableDownloadMixin(Table, Downloader):
     registry = defaultdict(dict)
 
     def __init__(self, group, name, url, fileName):
-        Table.__init__(self, [])
+        Table.__init__(self, [], [])
         TableDownloadMixin.registry[group][name] = self
         self.group = group
         self.name = name
